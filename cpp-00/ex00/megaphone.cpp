32cdba2f729df5a3f5a27577bc4c8497abfd8195
ft-Ali <alsiavos@student.42.fr>
2024-11-01 14:02:56 +0100

    Makefile done

--- cpp-00/ex00/megaphone.cpp
+++ cpp-00/ex00/megaphone.cpp
@@ -6,35 +6,31 @@
 /*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 16:34:15 by alsiavos          #+#    #+#             */
-/*   Updated: 2024/10/31 17:41:35 by alsiavos         ###   ########.fr       */
+/*   Updated: 2024/11/01 14:01:15 by alsiavos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-const std::string RESET = "\033[0m";
-const std::string RED = "\033[31m";
-const std::string GREEN = "\033[32m";
-const std::string YELLOW = "\033[33m";
-const std::string BLUE = "\033[34m";
-const std::string MAGENTA = "\033[35m";
-const std::string CYAN = "\033[36m";
-const std::string WHITE = "\033[37m";
 
-int	main(int c, char **v)
-{
-	if (c == 1)
-	{
+std::string RESET = "\033[0m";
+std::string RED = "\033[31m";
+std::string GREEN = "\033[32m";
+std::string YELLOW = "\033[33m";
+std::string BLUE = "\033[34m";
+std::string MAGENTA = "\033[35m";
+std::string CYAN = "\033[36m";
+std::string WHITE = "\033[37m";
+
+int	main(int c, char **v) {
+	if (c == 1) {
 		std::cout << RED << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << RESET << std::endl;
 		return (0);
-	}
-	else
-	{
-		for(int i = 1; i < c; i++)
-		{
+	} else {
+		for(int i = 1; i < c; i++) {
 			for (int j = 0; v[i][j]; j++)
-				std::cout << BLUE << (char)toupper(v[i][j]) << RESET;
+				std::cout << YELLOW << (char)toupper(v[i][j]) << RESET;
 		}
 	}
 	std::cout << std::endl;
