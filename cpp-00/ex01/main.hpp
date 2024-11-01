cd8dd4b3ddb2c7c1e0ea773006219b9ec5df8f70
ft-Ali <alsiavos@student.42.fr>
2024-11-01 15:24:17 +0100

    phonebook setup

---
+++ cpp-00/ex01/main.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 14:08:35 by alsiavos          #+#    #+#             */
+/*   Updated: 2024/11/01 14:10:40 by alsiavos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
+# define BLUE "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+# define WHITE "\033[37m"
+
+#include <iostream>
+#include <cctype>
+
+#endif
