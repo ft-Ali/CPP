52ac3dd6ae0432d247b39f8b85fb7654d983685e
ft-Ali <alsiavos@student.42.fr>
2024-11-26 18:59:13 +0100

    cpp-04 setup

---
+++ cpp-04/ex00/Animal.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 18:43:31 by alsiavos          #+#    #+#             */
+/*   Updated: 2024/11/26 18:44:51 by alsiavos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include "Color.hpp"
+# include <fstream>
+# include <iostream>
+# include <string>
+
+class Animal {
+	protected:
+		std::string _type;
+};
+
+
+#endif
